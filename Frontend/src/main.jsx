7da293f117fa7c9b47187c3228fe284d99c83c69
M4dh4v <discordboosterm@gmail.com>
2025-06-21 14:02:14 +0530

    New Algorithm for the estimation

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { DarkModeProvider } from './components/DarkModeContext'; // Import the provider
 import './index.css';
+import {Analytics} from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DarkModeProvider>
       <App />
+      <Analytics />
     </DarkModeProvider>
   </React.StrictMode>
 );
